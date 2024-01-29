601f576127afdead9b1b1e6327ff09fb6d5a53d2
DanielSaAmaro <121978184+DanielSaAmaro@users.noreply.github.com>
2024-01-29 15:25:53 -0700

    Implemented Probuilder and fancyfied code
    nice

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,15 +5,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Set transform for checking if player is touching the ground.
     public Transform groundCheckTransform = null;
+    // Keep track of wheter jump key was pressed.
     private bool jumpKeyWasPressed;
+    // Get horizontal input.
     private float horizontalInput;
+    // Set rigidbody to be used.
     private Rigidbody rigidBodyComponent;
-    private int jumpsRemaining = 2;    
+
+    // Define *total* amount of jumps player can perform before having to touch grass again.
+    private int jumpsTotal = 2;
+    // Define *current* amount of jumps player can perform before having to touch grass again. (Defined on start event)
+    private int jumpsRemaining;
+    // Define object's walk speed.
+    private float walkSpeed = 5.0f;
+    // Define object's jump speed.
+    private float jumpSpeed = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
+        // Define rigidbody as object's rigidbody component.
         rigidBodyComponent = GetComponent<Rigidbody>();
+        // Set jumps remaining as total jumps.
+        jumpsRemaining = jumpsTotal;
     }
 
     // Update is called once per frame
@@ -23,7 +39,8 @@ public class Player : MonoBehaviour
         {
             jumpKeyWasPressed = true;
         }
-        horizontalInput = Input.GetAxis("Horizontal");
+        // Set horizontal direction to be input * walk speed.
+        horizontalInput = Input.GetAxis("Horizontal")*walkSpeed;
     }
 
     private void FixedUpdate()
@@ -33,16 +50,20 @@ public class Player : MonoBehaviour
         {
             return;
         }
-
+        // If player touches ground, reset amount of jumps that can be performed.
         if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f).Length > 2)
         {
-            jumpsRemaining = 2;
+            // Reset remaining jumps as total jumps.
+            jumpsRemaining = jumpsTotal;
         }
-
+        
         if (jumpKeyWasPressed)
         {
-            rigidBodyComponent.AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+            // Set upwards jump force as up vector * jump speed.
+            rigidBodyComponent.AddForce(Vector3.up * jumpSpeed, ForceMode.VelocityChange);
+            // Define jump key as not pressed.
             jumpKeyWasPressed = false;
+            // Decrese amount of jumps left to jump.
             jumpsRemaining--;
         }
 
