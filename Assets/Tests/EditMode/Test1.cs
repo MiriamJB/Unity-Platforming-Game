380dd941457a896d13d667a11839cfe63aa996de
MiriamJB <mjb376@ensign.edu>
2024-01-31 14:42:25 -0700

    Creating Test files

---
+++ Assets/Tests/EditMode/Test1.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class Test1 {
+    [Test] // A Test behaves as an ordinary method
+    public void Test1SimplePasses() {
+        // Use the Assert class to test conditions
+    }
+
+}
