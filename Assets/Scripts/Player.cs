20075f993017620756526d490d8f482c794b9943
MiriamJB <mjb376@ensign.edu>
2024-02-13 10:53:05 -0700

    bug fixes
    - fix triple jump
    - fix extra jump after landing
    - update documentation comments in Player and animationStateController
    - adjust land-to-run animation timing

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,37 +1,32 @@
+using System;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour {
-    // Set transform for checking if player is touching the ground.
-    public Transform groundCheckTransform = null;
-    // Keep track of wheter jump key was pressed.
-    public bool jumpKeyWasPressed;
-    // Get horizontal input.
-    private float horizontalInput;
-    // Set rigidbody to be used.
-    private Rigidbody rigidBodyComponent;
+    private Rigidbody rigidBodyComponent; // Set rigidbody to be used.
+    public Transform groundCheckTransform = null; // Set transform for checking if player is touching the ground.
 
-    // Define *total* amount of jumps player can perform before having to touch grass again.
-    public int jumpsTotal = 2;
-    // Define *current* amount of jumps player can perform before having to touch grass again. (Defined on start event)
-    public int jumpsRemaining;
-    // Define object's walk speed.
-    private float walkSpeed = 4.0f;
-    // Define object's jump speed.
-    private float jumpSpeed = 5.0f;
-    public bool inAir;
+    // Jump vairables
+    public int objectsPlayerIsTouching; // used to keep track of jumps and if the player is falling
+    public bool jumpKeyWasPressed; // Keep track of whether jump key was pressed.
+    public int jumpsTotal = 2; // Define *total* amount of jumps player can perform before having to touch grass again.
+    public int jumpsRemaining; // Define *current* amount of jumps player can perform before having to touch grass again. (Defined on start event)
+    private float jumpSpeed = 5.0f; // Define object's jump speed.
+    public bool inAir; // used to control animations in animationStateController.cs
 
-    public GameObject footstepSound;
+    // Movement variables
+    public float horizontalInput; // Get horizontal input.
+    private float walkSpeed = 4.0f; // Define object's walk speed.
+    public GameObject footstepSound; // used for sound of footsteps
 
 
     // Start is called before the first frame update
     void Start() {
-        // Define rigidbody as object's rigidbody component.
-        rigidBodyComponent = GetComponent<Rigidbody>();
-        // Set jumps remaining as total jumps.
-        jumpsRemaining = jumpsTotal;
-        inAir = true;
+        rigidBodyComponent = GetComponent<Rigidbody>(); // Define rigidbody as object's rigidbody component.
+        jumpsRemaining = jumpsTotal; // Set jumps remaining as total jumps.
+        inAir = true; // the player starts out in the air
 
+        // define the state of footstepSound
         if (footstepSound != null) {
             footstepSound.SetActive(false);
         }
@@ -39,60 +34,58 @@ public class Player : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        // pressing the space bar will trigger a jump
+        if (Input.GetKeyDown(KeyCode.Space) && jumpsRemaining > 0) {
             jumpKeyWasPressed = true;
             inAir = true;
         }
-        // Set horizontal direction to be input * walk speed.
-        horizontalInput = Input.GetAxis("Horizontal") * walkSpeed;
 
+        horizontalInput = Input.GetAxis("Horizontal") * walkSpeed; // Set horizontal direction to be input * walk speed.
+
+        // footstep sound will start when the player is touching the ground and moving, and will stop otherwise
         if (Mathf.Abs(horizontalInput) > 0 && Physics.OverlapSphere(groundCheckTransform.position, 0.1f).Length > 2) {
             Footstep();
         }
         else {
             StopFootsteps();
         }
-
     }
 
+    // FixedUpdate is called at a set rate (doesn't fluctuate based on frame rate like the Update method)
     private void FixedUpdate() {
+        objectsPlayerIsTouching = Physics.OverlapSphere(groundCheckTransform.position, 0.1f).Length; // define how many objects player is touching
+        rigidBodyComponent.velocity = new Vector3(horizontalInput, rigidBodyComponent.velocity.y, 0); // move the player left & right based off of horizontalInput
 
-
-        rigidBodyComponent.velocity = new Vector3(horizontalInput, rigidBodyComponent.velocity.y, 0);
-
-
-        if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f).Length <= 2 && jumpsRemaining <= 0) {
+        // if the player has no jumps left and is not touching any other objects, return from the method
+        if (objectsPlayerIsTouching <= 2 && jumpsRemaining <= 0) {
             return;
         }
 
         // If player touches ground, reset amount of jumps that can be performed.
-        if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f).Length > 2) {
-            // Reset remaining jumps as total jumps.
-            jumpsRemaining = jumpsTotal;
+        if (objectsPlayerIsTouching > 2) {
+            jumpsRemaining = jumpsTotal; // Reset remaining jumps as total jumps.
             inAir = false;
-        }
-        else if (!inAir) {
-            inAir = true;
+        } else if (!inAir) {
+            inAir = true; // player is in air if touching less than 2 objects
         }
 
+        // make the player jump if the space bar was pressed
         if (jumpKeyWasPressed) {
-            // Cancel out gravity when peforming any kind of jump.
-            rigidBodyComponent.velocity = new Vector3(horizontalInput, 0, 0);
-            // Set upwards jump force as up vector * jump speed.
-            rigidBodyComponent.AddForce(Vector3.up * jumpSpeed, ForceMode.VelocityChange);
-            // Define jump key as not pressed.
-            jumpKeyWasPressed = false;
-            // Decrese amount of jumps left to jump.
-            jumpsRemaining--;
+            rigidBodyComponent.velocity = new Vector3(horizontalInput, 0, 0); // Cancel out gravity when peforming any kind of jump.
+            rigidBodyComponent.AddForce(Vector3.up * jumpSpeed, ForceMode.VelocityChange); // Set upwards jump force as up vector * jump speed.
+            jumpKeyWasPressed = false; // Define jump key as not pressed.
+            jumpsRemaining--; // Decrese amount of jumps left to jump.
         }
     }
 
+    // play the footstep sound
     void Footstep() {
         if (footstepSound != null && !footstepSound.activeSelf) {
             footstepSound.SetActive(true);
         }
     }
 
+    // stop the footstep sound
     void StopFootsteps() {
         if (footstepSound != null && footstepSound.activeSelf) {
             footstepSound.SetActive(false);
