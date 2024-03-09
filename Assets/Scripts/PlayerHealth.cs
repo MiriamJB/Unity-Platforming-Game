858457ac988161c62b09423ebfadba49fd59b6e2
MiriamJB <mjb376@ensign.edu>
2024-03-09 13:05:14 -0700

    make die good

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,25 +1,21 @@
 using UnityEngine;
 
-public class PlayerHealth : MonoBehaviour
-{
+public class PlayerHealth : MonoBehaviour {
     public int maxHealth;
     public int health;
     public Player player;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         health = maxHealth;
     }
-    public void TakeDamage(int damage)
-    {
-        if(player.invulnerability == false){
+    
+    public void TakeDamage(int damage) {
+        if (player.invulnerability == false) {
             health -= damage;
             player.ActivateInvulnerability();
-            if (health <= 0){
-                //Destroy(gameObject);
-                player.canMove = false;
-                player.isDead = true;
+            if (health <= 0) {
+                player.Die();
             }
         }
     }
