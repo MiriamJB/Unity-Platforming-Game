6d863560d7f72cd6ae85962f0d948f0d68735c17
DanielSaAmaro <121978184+DanielSaAmaro@users.noreply.github.com>
2024-03-11 15:00:43 -0600

    Updated scripts to use Player.cs Die method

--- Assets/Scripts/OutOfBounds.cs
+++ Assets/Scripts/OutOfBounds.cs
@@ -3,10 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class OutOfBounds : MonoBehaviour
 {
+    public Player player;
 
     void OnTriggerExit(Collider other) {
         if (other.gameObject.tag == "Player") {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name); // restart scene
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().name); // restart scene
+            player.Die();
         }
     }
 
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -33,9 +33,11 @@ public class PlayerHealth : MonoBehaviour
             yield return new WaitForSeconds(2f); // Wait for two seconds before each deduction check
 
             // Check if oxygen index is -1 and start deducting hearts
-            if (oxygenDisplay.currentOxygenIndex == -1)
+            if (oxygenDisplay.currentOxygenIndex <= 0)
             {
                 startDeductingHearts = true;
+            } else {
+                startDeductingHearts = false;
             }
 
             // Deduct hearts if the flag is set
@@ -55,8 +57,9 @@ public class PlayerHealth : MonoBehaviour
         if (health <= 0)
         {
             //Destroy(gameObject);
-            player.canMove = false;
-            player.isDead = true;
+            //player.canMove = false;
+            //player.isDead = true;
+            player.Die();
         }
     }
 
