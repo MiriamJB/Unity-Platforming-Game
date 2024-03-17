f5216a154bba3576f1ddda6904ff53190518074b
moronizacmel <moronizacmel@hotmail.com>
2024-03-17 17:45:50 -0600

    Fixed AirFeature bugs

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -12,7 +12,6 @@ public class PlayerHealth : MonoBehaviour
     public Image[] hearts;
     public Text gameOverText;
     private OxygenDisplay oxygenDisplay;
-    private bool startDeductingHearts = false; // Flag to indicate if hearts deduction should start
     
 
     // Start is called before the first frame update
@@ -24,37 +23,12 @@ public class PlayerHealth : MonoBehaviour
         oxygenDisplay = FindObjectOfType<OxygenDisplay>();
 
         // Start the coroutine to apply damage when oxygen level is -1
-        StartCoroutine(TakeDamageOverTime());
         UpdateCameraUI();
     }
 
     void UpdateCameraUI() {
         if (health <= 0) {
             gameOverText.text = "Game Over";
-        }
-    }
-
-    // Coroutine to apply damage when oxygen level is -1
-    IEnumerator TakeDamageOverTime()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(2f); // Wait for two seconds before each deduction check
-
-            // Check if oxygen index is -1 and start deducting hearts
-            if (oxygenDisplay.currentOxygenIndex <= 0)
-            {
-                startDeductingHearts = true;
-            } else {
-                startDeductingHearts = false;
-            }
-
-            // Deduct hearts if the flag is set
-            if (startDeductingHearts)
-            {
-                TakeDamage(1); // Deduct 1 heart
-                UpdateHeartsDisplay(); // Update the hearts display
-            }
         }
     }
 
